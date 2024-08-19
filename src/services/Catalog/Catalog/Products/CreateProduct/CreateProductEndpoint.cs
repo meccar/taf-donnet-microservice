70d943f9f1163855e92e40d1b18dd4172e44daed
meccar <tung14hn@gmail.com>
2024-08-19 21:35:51 +0800

    feat: updated CQRS and func

--- src/services/Catalog/Catalog/Products/CreateProduct/CreateProductEndpoint.cs
+++ src/services/Catalog/Catalog/Products/CreateProduct/CreateProductEndpoint.cs
@@ -1,5 +1,9 @@
 ﻿namespace Catalog.Products.CreateProduct
 {
+    public record CreateProductRequest(string name, List<string> Category, string Description, string ImageFile, decimal Price)
+
+    public record CreateProductResponse(Guid Id);
+
     public class CreateProductEndpoint
     {
 
---
+++ src/services/Catalog/Catalog/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,29 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.Models;
+using MediatR;
+
+namespace Catalog.Products.CreateProduct
+{
+    public record CreateProductCommand(string name, List<string> Category, string Description, string ImageFile, decimal Price): ICommand<CreateProductResult>;
+
+    public record CreateProductResult(Guid Id);
+
+    internal class CreateProductHandler: ICommandHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = command.Name,
+                CategoryAttribute = command.Category,
+                DescriptionAttribute = command.Description,
+                ImageFileMachine = command.ImageFile,
+                Price = command.Price
+
+            };
+
+            return new CreateProductResult(Guid.NewGuid());
+
+        }
+    }
+}
