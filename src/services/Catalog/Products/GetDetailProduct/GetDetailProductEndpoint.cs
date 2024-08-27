39e09152810688d0e1fc426be3099fa68f62ef27
meccar <tung14hn@gmail.com>
2024-08-27 22:33:58 +0800

    feat: implemented detail product

--- src/services/Catalog/Products/GetDetailProduct/GetDetailProductEndpoint.cs
+++ src/services/Catalog/Products/GetDetailProduct/GetDetailProductEndpoint.cs
@@ -1,7 +1,24 @@
 ﻿namespace Catalog.Products.GetDetailProduct
 {
-
+    //public record GetDetailProductRequest();
+    public record GetDetailProductResponse(Product Product);
     public class GetDetailProductEndpoint
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new GetDetailProductQuery(id));
+
+                var response = result.Adapt<GetDetailProductResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("GetDetailProdct")
+            .Produces<GetDetailProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Detail Product")
+            .WithDescription("Get Detail Product");
+        }
     }
 }
